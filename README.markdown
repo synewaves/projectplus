5919e9a9942d2498abe804f99efe3cdb96d602f2
Mark Huot <mark@markhuot.com>
2011-05-14 18:41:09 -0400

    removing it all from the pages branch

--- README.markdown
+++
@@ -1,3 +0,0 @@
-![Screen Shot](http://github.com/markhuot/projectplus/raw/master/releases/shot.png)
-
-An updated version ready for Lion.
--- src/etc/TextMate.h
+++
@@ -1,7 +0,0 @@
-#import "JRSwizzle.h"
-
-#define OakProjectController     NSClassFromString(@"OakProjectController")
-#define OakPreferencesManager    NSClassFromString(@"OakPreferencesManager")
-#define OakOutlineView           NSClassFromString(@"OakOutlineView")
-#define OakMenuButton            NSClassFromString(@"OakMenuButton")
-#define OakWindow                NSClassFromString(@"OakWindow")
